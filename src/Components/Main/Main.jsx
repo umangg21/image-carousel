5be243d6f70ef50b3db2eae32866d5250be384a2
Umang Gupta <umang@indwealth.in>
2020-06-04 13:08:12 +0530

    Add custom boilerplate

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./Main.scss";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
+import Main from './Components/Main/Main';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
