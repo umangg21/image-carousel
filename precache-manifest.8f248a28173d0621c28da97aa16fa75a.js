eecdd20e5eef65b6a9a6a4deb8657de338414a62
Umang Gupta <umang@indwealth.in>
2020-06-05 23:18:03 +0530

    Updates

---
+++ precache-manifest.8f248a28173d0621c28da97aa16fa75a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215033264ffd2d0d9d52f9e48085e466",
+    "url": "/image-carousel/index.html"
+  },
+  {
+    "revision": "73d1dabceea4fbcf98cb",
+    "url": "/image-carousel/static/css/main.63de8f3a.chunk.css"
+  },
+  {
+    "revision": "d78ae9b3ebe2c6460d5e",
+    "url": "/image-carousel/static/js/2.e6c0a5a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-carousel/static/js/2.e6c0a5a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d1dabceea4fbcf98cb",
+    "url": "/image-carousel/static/js/main.5ae72105.chunk.js"
+  },
+  {
+    "revision": "8a0125c027072dc66c2f",
+    "url": "/image-carousel/static/js/runtime-main.b2afb9b5.js"
+  }
+]);
